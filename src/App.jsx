b0ed930b14afce801059b4aec9f9471318d9a616
Niedja92 <niedleticia@hotmail.com>
2023-11-08 17:17:18 -0300

    creating-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Main from "./components/Main"
+import Navbar from "./components/Navbar"
 
 function App() {
 
@@ -7,6 +9,8 @@ function App() {
     <>
       <Header />
       <Main />
+      <Navbar />
+      <Footer />
     </>
   )
 }
