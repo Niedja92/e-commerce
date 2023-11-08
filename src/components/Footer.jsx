7a30f8b6c29135658597bab853216fec57a26608
Niedja92 <niedleticia@hotmail.com>
2023-11-08 17:23:03 -0300

    adding-images-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
+import Boleto from "../assets/images-pay/boleto.png"
+import Hipercard from "../assets/images-pay/hipercard.png"
+import Mastercard from "../assets/images-pay/master_card.png"
+import Visa from "../assets/images-pay/visa.png"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+        Formas de pagamento:
+        <img src={Boleto} />
+        <img src={Hipercard} />
+        <img src={Mastercard} />
+        <img src={Visa} />
+    </div>
   )
 }
 
