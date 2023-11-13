dd396239e019f70f35bfd93ab35b2e8d62604168
Daniele Barbosa <bdsdaniele22@gmail.com>
2023-11-13 19:23:51 -0300

    boot

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,53 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
-const components = () => {
+
+const Navbar = () => {
   return (
-    <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-      {/* Conteúdo da barra de navegação */}
+    <nav className="navbar navbar-dark bg-dark fixed-top">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Black Friday</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+          
+
+        </button>
+        <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
+            <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu dropdown-menu-dark">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li>
+                    <hr className="dropdown-divider"/>
+                  </li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form className="d-flex mt-3" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+              <button className="btn btn-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </nav>
   )
-};
-export default Navbar
+}
+
+export default Navbar;
