c48f6724916b5c33ddc4c3c7933dde8405d207d0
sofia <sofia-lorena13@hotmail.com>
2023-11-14 17:10:39 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,17 +2,30 @@ import Boleto from "../assets/images-pay/boleto.png"
 import Hipercard from "../assets/images-pay/hipercard.png"
 import Mastercard from "../assets/images-pay/master_card.png"
 import Visa from "../assets/images-pay/visa.png"
+import "../styles/styles.css"
 
 const Footer = () => {
   return (
+    
     <div>
-        Formas de pagamento:
+      <div className="footer">
+          <span>Formas de pagamento</span>
+        </div>
+      <div className="pagamento">
         <img src={Boleto} />
         <img src={Hipercard} />
         <img src={Mastercard} />
-        <img src={Visa} />
+        <img src={Visa} />  
     </div>
+
+      <div>
+        <span>&copy; 2023</span>
+      </div>  
+         
+        </div>
+        
   )
+  
 }
 
 export default Footer
